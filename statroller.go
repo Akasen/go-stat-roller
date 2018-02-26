1cc8c0c9656b5f82986577d681e0332d05f7d99b
Joshua <akasen1226@gmail.com>
2018-02-26 17:59:12 -0500

    first commit

---
+++ statroller.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the Stat Roller!")
+	fmt.Println("This Stat Roller can use the #@$&#$*#&")
+
+	getChoice()
+}
+
+func getChoice() {
+	fmt.Println("\nINPUT SOMETHING")
+
+}
+
+func roll() {
+	fmt.Println("ROLL ROLL ROLL")
+
+}
