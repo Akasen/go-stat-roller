dd5f65981491c27f43ab2025348169a2df5c79af
Joshua <akasen1226@gmail.com>
2018-02-26 18:18:13 -0500

    test commit from Visual Studio Source

--- statroller.go
+++ statroller.go
@@ -4,19 +4,17 @@ import (
 	"fmt"
 )
 
+func getChoice() {
+	fmt.Println("\nINPUT SOMETHING")
+}
+
+func roll() {
+	fmt.Println("ROLL ROLL ROLL")
+}
+
 func main() {
 	fmt.Println("Welcome to the Stat Roller!")
 	fmt.Println("This Stat Roller can use the #@$&#$*#&")
 
 	getChoice()
-}
-
-func getChoice() {
-	fmt.Println("\nINPUT SOMETHING")
-
-}
-
-func roll() {
-	fmt.Println("ROLL ROLL ROLL")
-
 }
